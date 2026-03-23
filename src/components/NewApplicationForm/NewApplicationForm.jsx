2469ae94481d9647cdb845842195a26bb807200d
David Arrazola <arrazola.dvd@gmail.com>
2026-03-23 18:56:05 -0400

    ADD functionality to form. Users can now create a new application and it saves to database

--- src/components/NewApplicationForm/NewApplicationForm.jsx
+++ src/components/NewApplicationForm/NewApplicationForm.jsx
@@ -1,61 +1,125 @@
-//TODO ADD BETTER COMMENTS
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./NewApplicationForm.css";
 
 export default function NewApplicationForm() {
-  const [company, setCompany] = useState("");
-  const [role, setRole] = useState("");
-  const [status, setStatus] = useState("");
-  const [jobUrl, setJobUrl] = useState("");
-  const [dateApplied, setdateApplied] = useState("");
-  const [notes, setNotes] = useState("");
-  const [contactName, setContactName] = useState("");
-  const [contactEmail, setContactEmail] = useState("");
+  const [formData, setFormData] = useState({
+    company: "",
+    role: "",
+    status: "",
+    jobUrl: "",
+    dateApplied: "",
+    notes: "",
+    contactName: "",
+    contactEmail: "",
+  });
+
+  const API = import.meta.env.VITE_API_URL;
+  const token = localStorage.getItem("token");
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const createNewApp = async (event) => {
+    // event.preventDefault();
+
+    try {
+      const res = await fetch(`${API}/applications`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!res.ok) throw new Error("Failed request");
+
+      const data = await res.json();
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <section className="formOverlay">
-      <form className="formContent">
+      <form className="formContent" onSubmit={createNewApp}>
         <h3>New Application</h3>
 
         <section className="formGrid">
           <label>
             Company
-            <input type="text" />
+            <input
+              name="company"
+              value={formData.company}
+              onChange={handleChange}
+            />
           </label>
 
           <label>
             Role
-            <input type="text" />
+            <input name="role" value={formData.role} onChange={handleChange} />
           </label>
 
           <label>
             Status
-            <input type="text" />
+            <input
+              name="status"
+              value={formData.status}
+              onChange={handleChange}
+            />
           </label>
 
           <label>
             Job URL
-            <input type="url" />
+            <input
+              type="url"
+              name="jobUrl"
+              value={formData.jobUrl}
+              onChange={handleChange}
+            />
           </label>
 
           <label>
             Date Applied
-            <input type="date" />
+            <input
+              type="date"
+              name="dateApplied"
+              value={formData.dateApplied}
+              onChange={handleChange}
+            />
           </label>
 
           <label>
             Notes
-            <input type="text" />
+            <input
+              name="notes"
+              value={formData.notes}
+              onChange={handleChange}
+            />
           </label>
 
           <label>
             Contact Name
-            <input type="text" />
+            <input
+              name="contactName"
+              value={formData.contactName}
+              onChange={handleChange}
+            />
           </label>
 
           <label>
             Contact Email
-            <input type="email" />
+            <input
+              type="email"
+              name="contactEmail"
+              value={formData.contactEmail}
+              onChange={handleChange}
+            />
           </label>
         </section>
 
