8bc5faae90e4a8595036da2167c93a442e996b26
David Arrazola <arrazola.dvd@gmail.com>
2026-03-23 21:00:27 -0400

    ADD a new file; applicationsContext.jsx. Used to retrieved application list only once and use it in different pages when needed, instead of having multiple pages call api many times

--- src/components/Applications.jsx
+++ src/components/Applications.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router";
 import { useEffect, useState } from "react";
-import { api } from "../services/api";
+// import { api } from "../services/api";
 import NewApplicationForm from "./NewApplicationForm/NewApplicationForm";
 
 export default function Applications() {
